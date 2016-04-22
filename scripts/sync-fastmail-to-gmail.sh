b2795cf8a650ee92728883c94a08feccad795295
Neil Grogan <neil@ngrogan.com>
2016-04-22 10:58:20 +0100

    Added more options to gmail fastmail script

--- scripts/sync-fastmail-to-gmail.sh
+++ scripts/sync-fastmail-to-gmail.sh
@@ -9,7 +9,15 @@ $IMAP_SYNC \
 --host2 $HOST2 --user2 $USER2 --password2 $PASS2 -ssl2 \
 --prefix1 "INBOX." \
 --prefix2 "[Gmail]" \
+--exitwhenover 500000000 \
+--maxsize 25000000 \
 --automap \
+--expunge1 \
+--addheader \
+--exclude "\[Gmail\]$" \
+--regextrans2 "s/[ ]+/_/g" \
+--regextrans2 "s/[\^]/_/g" \
+--regextrans2 "s/['\"\\\\]/_/g"
 --justfolders # trial run for now
 # --regextrans2 's/INBOX\.Archive/All Mail/'
 # --useheader 'Message-Id' --skipsize
