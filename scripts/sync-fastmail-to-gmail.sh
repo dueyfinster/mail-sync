6290e6b1b5af794f667ff8a6ce92b0d97ce082ac
Neil Grogan <neil@ngrogan.com>
2016-04-22 10:55:54 +0100

    Updated Fastmail Gmail Script

--- scripts/sync-fastmail-to-gmail.sh
+++ scripts/sync-fastmail-to-gmail.sh
@@ -7,4 +7,9 @@ HOST2="imap.gmail.com"
 $IMAP_SYNC \
 --host1 $HOST1 --user1 $USER1 --password1 $PASS1 -ssl1 \
 --host2 $HOST2 --user2 $USER2 --password2 $PASS2 -ssl2 \
+--prefix1 "INBOX." \
+--prefix2 "[Gmail]" \
+--automap \
 --justfolders # trial run for now
+# --regextrans2 's/INBOX\.Archive/All Mail/'
+# --useheader 'Message-Id' --skipsize
