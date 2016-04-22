e4708c4b714bf6ef55db757d07b6982766037942
Neil Grogan <neil@ngrogan.com>
2016-04-22 10:48:01 +0100

    Updated Fastmal/Gmail script to use ssl, fixed typo

--- scripts/sync-fastmail-to-gmail.sh
+++ scripts/sync-fastmail-to-gmail.sh
@@ -5,6 +5,6 @@ HOST2="imap.gmail.com"
 
 
 $IMAP_SYNC \
---host1 $HOST1 --user1 $USER1 --password1 $PASS1 \
---host2 $HOST2 --user2 $USER2 --password1 $PASS2 \
+--host1 $HOST1 --user1 $USER1 --password1 $PASS1 -ssl1 \
+--host2 $HOST2 --user2 $USER2 --password2 $PASS2 -ssl2 \
 --justfolders # trial run for now
